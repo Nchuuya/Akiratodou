0959e5eabed22e906daa98b6c835d588b2033c9c
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-04-19 20:43:59 +0530

    Update script.py

--- SiestaRobot/script.py
+++ SiestaRobot/script.py
@@ -28,6 +28,6 @@ MIKU_N_IMG = (
 
 )
 
-PM_PHOTO = "https://telegra.ph/file/262cf5bb9780cbb96afb7.jpg"
+PM_PHOTO = "https://telegra.ph/file/a319c4b912f2dfd24f17f.jpg"
 
 MIKU_DISPACHER_PIC = "https://telegra.ph/file/b3d283f1040d5a2ce8aac.jpg"
