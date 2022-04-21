b57ba15172d1bd458bf142fb174263a56a1f9cbc
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-04-22 00:07:15 +0530

    Update truth_and_dare.py

--- SiestaRobot/modules/truth_and_dare.py
+++ SiestaRobot/modules/truth_and_dare.py
@@ -28,6 +28,10 @@ def onepiece(update: Update, context: CallbackContext):
     update.effective_message.reply_photo(random.choice(truth_and_dare_string.WAIFU))
 
 
+def reels(update: Update, context: CallbackContext):
+    update.effective_message.reply_video(random.choice(truth_and_dare_string.REELS))
+
+
 
 def tord(update: Update, context: CallbackContext):
     args = context.args
@@ -46,6 +50,7 @@ COSPLAY_HANDLER = DisableAbleCommandHandler("cosplay", cosplay, run_async=True)
 WAIFU_HANDLER = DisableAbleCommandHandler("onepiece", onepiece, run_async=True)
 WYR_HANDLER = DisableAbleCommandHandler("wyr", wyr, run_async=True)
 TORD_HANDLER = DisableAbleCommandHandler("tord", tord, run_async=True)
+REELS_HANDLER = DisableAbleCommandHandler("tord", tord, run_async=True)
 
 
 
@@ -58,3 +63,4 @@ dispatcher.add_handler(COSPLAY_HANDLER)
 dispatcher.add_handler(WAIFU_HANDLER)
 dispatcher.add_handler(WYR_HANDLER)
 dispatcher.add_handler(TORD_HANDLER)
+dispatcher.add_handler(REELS_HANDLER)
