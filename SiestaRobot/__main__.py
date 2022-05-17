a0d3a988e21a7c077d633b4c11345b09b57024ad
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-17 19:46:41 +0530

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -212,7 +212,7 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text=gs(chat.id, "owner_button"), url=https://t.me/DarkkCarnage"),
+                            InlineKeyboardButton(text=gs(chat.id, "owner_button"), url="https://t.me/DarkkCarnage"),
                         ],
                         [
                             InlineKeyboardButton(text=gs(chat.id, "help_button"), callback_data="help_back"),
