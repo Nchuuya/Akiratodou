1c100345cfea61825116eaba35c5f3b86ef95782
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-05-17 15:44:00 +0530

    Update userinfo.py

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -450,21 +450,15 @@ def stats(update, context):
     status += "*✦ System Start time:* " + str(uptime) + "\n"
     uname = platform.uname()
     status += "*♡ System:* " + str(uname.system) + "\n"
-    status += "*✦ Node name:* " + escape_markdown(str(uname.node)) + "\n"
-    status += "*♡ Release:* " + escape_markdown(str(uname.release)) + "\n"
-    status += "*✦ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
     mem = virtual_memory()
     cpu = cpu_percent()
     disk = disk_usage("/")
     status += "*♡ CPU:* " + str(cpu) + " %\n"
     status += "*✦ RAM:* " + str(mem[2]) + " %\n"
     status += "*♡ Storage:* " + str(disk[3]) + " %\n\n"
-    status += "*✦ Python Version:* " + python_version() + "\n"
-    status += "*♡ python-Telegram-Bot:* " + str(ptbver) + "\n"
-    status += "*✦ Uptime:* " + str(botuptime) + "\n"
     try:
-        update.effective_message.reply_photo(
-            MIKU_IMG,
+        update.effective_message.reply_text(
+            
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
